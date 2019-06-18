9f5e27edd33583bd550e6786016e10b3718f3fd8
Adam Burkenpas <aurkenpas@gmail.com>
2019-06-18 08:28:53 -0400

    Add test stub

---
+++ test/BridgeDownActions.test.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils'
+import BridgeDownActions from '~/components/BridgeDownActions.vue'
+
+describe('BridgeDownActions.vue', () => {
+  beforeEach(() => {
+    wrapper = shallowMount(BridgeDownActions, {})
+  })
+
+  it('message is equal to', () => {})
+})
